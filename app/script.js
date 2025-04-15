91cb06dce671fd3f883d1655d46dfd26e0322180
Patryk Olszewski <patrykolszewski420@gmail.com>
2025-04-15 14:47:01 +0200

    timerApp

---
+++ app/script.js
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import { render } from "react-dom";
+
+const App = () => {
+	const [status, setStatus] = useState("off");
+	const [time, setTime] = useState(20 * 60);
+	const [timer, setTimer] = useState(null);
+	const audioElement = new Audio("./sounds/bell.wav");
+
+	const formatTime = (time) => {
+		const minutes = Math.floor(time / 60);
+		const seconds = time % 60;
+		return `${minutes < 10 ? "0" : ""}${minutes}:${
+			seconds < 10 ? "0" : ""
+		}${seconds}`;
+	};
+
+	const startTimer = () => {
+		let currentStatus = "work";
+		setStatus(currentStatus);
+
+		const interval = setInterval(() => {
+			setTime((prevTime) => {
+				if (prevTime <= 1) {
+					audioElement.play();
+					if (currentStatus === "work") {
+						currentStatus = "rest";
+						setStatus("rest");
+						return 20; // 20 sekund przerwy
+					} else {
+						currentStatus = "work";
+						setStatus("work");
+						return 20 * 60; // 20 minut pracy
+					}
+				}
+				return prevTime - 1;
+			});
+		}, 1000);
+
+		setTimer(interval);
+	};
+
+	const stopTimer = () => {
+		clearInterval(timer);
+		setStatus("off");
+		setTime(20 * 60);
+	};
+
+	const closeApp = () => {
+		window.close();
+	};
+
+	return (
+		<div>
+			<h1>Protect your eyes</h1>
+
+			{status === "off" && (
+				<div>
+					<p>
+						According to optometrists, you should follow the 20/20/20 rule: rest
+						your eyes every 20 minutes by looking at something 20 feet away for
+						20 seconds.
+					</p>
+					<p>This app will help you remember to do that.</p>
+				</div>
+			)}
+
+			{status === "work" && <img src="./images/work.png" alt="Work" />}
+			{status === "rest" && <img src="./images/rest.png" alt="Rest" />}
+			{status !== "off" && <div className="timer">{formatTime(time)}</div>}
+
+			{status === "off" && (
+				<button className="btn" onClick={() => startTimer()}>
+					Start
+				</button>
+			)}
+			{status !== "off" && (
+				<button className="btn" onClick={() => stopTimer()}>
+					Stop
+				</button>
+			)}
+			<button className="btn btn-close" onClick={closeApp}>
+				X
+			</button>
+		</div>
+	);
+};
+
+render(<App />, document.querySelector("#app"));
